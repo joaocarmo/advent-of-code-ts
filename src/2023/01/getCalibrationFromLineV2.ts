4f1c8ee46f72ea85b16912dc7c67c3bc40482fd8
João Carmo <kingjoao@gmail.com>
2023-12-03 23:20:03 +0000

    feat(getCalibrationFromLineV2): add number spelling recognition functionality

--- src/2023/01/getCalibrationFromLineV2.ts
+++ src/2023/01/getCalibrationFromLineV2.ts
@@ -1,5 +1,65 @@
 import { isDigit } from "./isDigit"
 
+enum Direction {
+  left,
+  right,
+}
+
+enum NumberEnum {
+  zero = "0",
+  one = "1",
+  two = "2",
+  three = "3",
+  four = "4",
+  five = "5",
+  six = "6",
+  seven = "7",
+  eight = "8",
+  nine = "9",
+}
+
+type NumberMap = {
+  [key in keyof typeof NumberEnum]: NumberEnum
+}
+
+const numberMap: NumberMap = {
+  zero: NumberEnum.zero,
+  one: NumberEnum.one,
+  two: NumberEnum.two,
+  three: NumberEnum.three,
+  four: NumberEnum.four,
+  five: NumberEnum.five,
+  six: NumberEnum.six,
+  seven: NumberEnum.seven,
+  eight: NumberEnum.eight,
+  nine: NumberEnum.nine,
+}
+
+const MAX_KEY_LENGTH = 5
+
+const getFromSpelled = (
+  line: string,
+  direction: Direction = Direction.left,
+): string | undefined => {
+  if (!line.length) {
+    return
+  }
+
+  const N = line.length
+  const K = Math.max(N, MAX_KEY_LENGTH)
+
+  for (let i = 0; i <= K; i++) {
+    const key =
+      direction === Direction.left
+        ? line.slice(0, i + 1)
+        : line.slice(N - 1 - i, N)
+
+    if (key in numberMap) {
+      return numberMap[key as keyof typeof numberMap]
+    }
+  }
+}
+
 export const getCalibrationFromLine = (line: string): number => {
   const N = line.length
   let calibration: string = ""
@@ -8,16 +68,34 @@ export const getCalibrationFromLine = (line: string): number => {
 
   for (let i = 0; i < N; i++) {
     const leftChar = line[i]
+    const leftRest = line.slice(i)
     const rightChar = line[N - 1 - i]
+    const rightRest = line.slice(0, N - i)
 
-    if (!foundLeftDigit && isDigit(leftChar)) {
-      calibration = `${leftChar}${calibration}`
-      foundLeftDigit = true
+    if (!foundLeftDigit) {
+      if (isDigit(leftChar)) {
+        calibration = `${leftChar}${calibration}`
+        foundLeftDigit = true
+      } else {
+        const leftCharFromSpelled = getFromSpelled(leftRest, Direction.left)
+        if (leftCharFromSpelled) {
+          calibration = `${leftCharFromSpelled}${calibration}`
+          foundLeftDigit = true
+        }
+      }
     }
 
-    if (!foundRightDigit && isDigit(rightChar)) {
-      calibration = `${calibration}${rightChar}`
-      foundRightDigit = true
+    if (!foundRightDigit) {
+      if (isDigit(rightChar)) {
+        calibration = `${calibration}${rightChar}`
+        foundRightDigit = true
+      } else {
+        const rightCharFromSpelled = getFromSpelled(rightRest, Direction.right)
+        if (rightCharFromSpelled) {
+          calibration = `${calibration}${rightCharFromSpelled}`
+          foundRightDigit = true
+        }
+      }
     }
 
     // We're looking for a 2-digit number
