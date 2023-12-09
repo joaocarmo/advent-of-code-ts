ab722a1785b0ba895aead20179fcf186c34283d5
João Carmo <kingjoao@gmail.com>
2023-12-09 18:25:24 +0000

    perf(solutionV2): Improve getNextNodes function using memoization

--- src/2023/08/solutionV2.ts
+++ src/2023/08/solutionV2.ts
@@ -1,31 +1,32 @@
+import { memoize } from "lodash"
 import { Direction } from "."
 import type { Network, Node, State } from "."
 
 const getInitialNodes = (network: Network): Node[] =>
   Object.keys(network).filter((node) => node.endsWith("A"))
 
-const getNextNodes = (
-  previousNodes: Node[],
-  network: Network,
-  direction: Direction,
-): Node[] => {
-  const nextNodes = [...previousNodes]
+const getNextNodes = memoize(
+  (previousNodes: Node[], network: Network, direction: Direction): Node[] => {
+    const nextNodes = [...previousNodes]
 
-  for (let i = 0; i < nextNodes.length; i++) {
-    let nextNode = nextNodes[i]
-    const [leftNode, rightNode] = network[nextNode]
+    for (let i = 0; i < nextNodes.length; i++) {
+      let nextNode = nextNodes[i]
+      const [leftNode, rightNode] = network[nextNode]
 
-    if (direction === Direction.Left) {
-      nextNode = leftNode
-    } else {
-      nextNode = rightNode
+      if (direction === Direction.Left) {
+        nextNode = leftNode
+      } else {
+        nextNode = rightNode
+      }
+
+      nextNodes[i] = nextNode
     }
 
-    nextNodes[i] = nextNode
-  }
-
-  return nextNodes
-}
+    return nextNodes
+  },
+  (previousNodes, _network, direction) =>
+    `${previousNodes.join("")}-${direction}`,
+)
 
 export const solution = (state: State): State => {
   const { input, network } = state
