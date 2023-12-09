da3bd957fc20617934ee9979370d990f8613f345
João Carmo <kingjoao@gmail.com>
2023-12-09 18:20:16 +0000

    refactor(solutionV2): Extract getNextNodes logic into separate function

--- src/2023/08/solutionV2.ts
+++ src/2023/08/solutionV2.ts
@@ -4,24 +4,36 @@ import type { Network, Node, State } from "."
 const getInitialNodes = (network: Network): Node[] =>
   Object.keys(network).filter((node) => node.endsWith("A"))
 
+const getNextNodes = (
+  previousNodes: Node[],
+  network: Network,
+  direction: Direction,
+): Node[] => {
+  const nextNodes = [...previousNodes]
+
+  for (let i = 0; i < nextNodes.length; i++) {
+    let nextNode = nextNodes[i]
+    const [leftNode, rightNode] = network[nextNode]
+
+    if (direction === Direction.Left) {
+      nextNode = leftNode
+    } else {
+      nextNode = rightNode
+    }
+
+    nextNodes[i] = nextNode
+  }
+
+  return nextNodes
+}
+
 export const solution = (state: State): State => {
   const { input, network } = state
   let currentIndex = 0
-  const currentNodes = getInitialNodes(network)
+  let currentNodes = getInitialNodes(network)
 
   while (!currentNodes.every((node) => node.endsWith("Z"))) {
-    for (let i = 0; i < currentNodes.length; i++) {
-      let currentNode = currentNodes[i]
-      const [leftNode, rightNode] = network[currentNode]
-
-      if (input[currentIndex] === Direction.Left) {
-        currentNode = leftNode
-      } else {
-        currentNode = rightNode
-      }
-
-      currentNodes[i] = currentNode
-    }
+    currentNodes = getNextNodes(currentNodes, network, input[currentIndex])
 
     state.numberOfSteps += 1
     currentIndex += 1
