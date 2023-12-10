d8897bca73af15a6b43c2e615d8620849bc12c3f
João Carmo <17721880+joaocarmo@users.noreply.github.com>
2023-12-10 23:31:59 +0000

    feat(2023): solution for day 8, part 2 (#19)
    * feat(index): Update challenge import to version 2023/08
    
    * refactor(solutionV2): Remove lodash memoize usage in getNextNodes function
    
    * build(package.json): Add compute-lcm to dependencies and devDependencies
    
    * refactor(2023/08): Move findSolution implementation to solution files

--- src/2023/08/solutionV1.ts
+++ src/2023/08/solutionV1.ts
@@ -1,7 +1,13 @@
 import { Direction } from "."
 import type { Node, State } from "."
 
-export const solution = (state: State): State => {
+export const findSolution = (state: State) => () => {
+  const { numberOfSteps } = solution(state)
+
+  console.log({ numberOfSteps })
+}
+
+const solution = (state: State): State => {
   const { input, network } = state
   let currentIndex = 0
   let currentNode: Node = "AAA"
@@ -14,8 +20,10 @@ export const solution = (state: State): State => {
     } else {
       currentNode = rightNode
     }
+
     state.numberOfSteps += 1
     currentIndex += 1
+
     if (currentIndex === input.length) {
       currentIndex = 0
     }
