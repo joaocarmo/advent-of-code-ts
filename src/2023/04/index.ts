ef9d13beb8978a851ef2f81c7781997dd796308b
João Carmo <kingjoao@gmail.com>
2023-12-10 12:30:42 +0000

    refactor(cards): Change cards to array of cards in State interface

--- src/2023/04/index.ts
+++ src/2023/04/index.ts
@@ -13,18 +13,18 @@ interface Card {
 
 interface State {
   cards: {
-    [key: CardId]: Card
+    [key: CardId]: Card[]
   }
 }
 
 const solution = (state: State): State => {
   for (const cardId in state.cards) {
-    const card = state.cards[cardId]
+    const card = state.cards[cardId][0]
 
     for (const numberWeHave of card.numbersWeHave) {
       if (card.winningNumbers.includes(numberWeHave)) {
-        state.cards[cardId].points = 2 ** state.cards[cardId].numOfMatches
-        state.cards[cardId].numOfMatches++
+        state.cards[cardId][0].points = 2 ** state.cards[cardId][0].numOfMatches
+        state.cards[cardId][0].numOfMatches++
       }
     }
   }
@@ -35,24 +35,26 @@ const solution = (state: State): State => {
 const parseLine = (state: State) => (line: string) => {
   const [cardId, numbers] = line.split(":")
   const [winningNumbers, numbersWeHave] = numbers.split("|")
-  state.cards[extractNumber(cardId)!] = {
-    winningNumbers: winningNumbers
-      .split(" ")
-      .filter((n) => !!n.trim())
-      .map(Number),
-    numbersWeHave: numbersWeHave
-      .split(" ")
-      .filter((n) => !!n.trim())
-      .map(Number),
-    numOfMatches: 0,
-    points: 0,
-  }
+  state.cards[extractNumber(cardId)!] = [
+    {
+      winningNumbers: winningNumbers
+        .split(" ")
+        .filter((n) => !!n.trim())
+        .map(Number),
+      numbersWeHave: numbersWeHave
+        .split(" ")
+        .filter((n) => !!n.trim())
+        .map(Number),
+      numOfMatches: 0,
+      points: 0,
+    },
+  ]
 }
 
 const findSolution = (state: State) => () => {
   const result = solution(state)
   const totalPoints = Object.values(result.cards).reduce(
-    (acc, card) => acc + card.points,
+    (acc, card) => acc + card[0].points,
     0,
   )
 
