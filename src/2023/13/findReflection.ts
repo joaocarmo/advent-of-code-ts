969441e9f574ee95327579ddb724efc04d4e4be7
João Carmo <kingjoao@gmail.com>
2023-12-17 12:38:45 +0000

    refactor(findReflection): Improve loop conditions and variable naming

--- src/2023/13/findReflection.ts
+++ src/2023/13/findReflection.ts
@@ -3,21 +3,14 @@ import { Pattern } from "."
 const findReflectionInArray = (arr: string[]): number | null => {
   const N = arr.length
 
-  for (let i = 0; i < N; i++) {
+  for (let i = 1; i < N; i++) {
     let isReflection = true
 
-    for (let j = 0; j < Math.floor(N / 2); j++) {
-      const leftIndex = i - j - 1
-      const rightIndex = i + j
-      const row1 = arr[leftIndex]
-      const row2 = arr[rightIndex]
+    for (let j = 0; j < Math.min(N - i, i); j++) {
+      const leftSide = arr[i - j - 1]
+      const rightSide = arr[i + j]
 
-      if (leftIndex < 0 || rightIndex >= N) {
-        isReflection = false
-        break
-      }
-
-      if (row1 !== row2) {
+      if (leftSide !== rightSide) {
         isReflection = false
         break
       }
