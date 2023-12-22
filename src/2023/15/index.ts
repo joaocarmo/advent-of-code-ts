372cc98a6d8ec76a9e9a42c5ba131211fbc9953b
João Carmo <kingjoao@gmail.com>
2023-12-22 03:00:00 +0000

    feat(Operations): Add operations handling and focusing power sum calculation

--- src/2023/15/index.ts
+++ src/2023/15/index.ts
@@ -6,10 +6,13 @@ enum Operation {
   "=" = "=",
 }
 
-interface StepInfo {
+interface Lens {
   label: string
-  operation: Operation
   focalLength: number
+}
+
+interface StepInfo extends Lens {
+  operation: Operation
 }
 
 interface Step extends StepInfo {
@@ -18,8 +21,22 @@ interface Step extends StepInfo {
   labelHash: number
 }
 
+type Boxes = Record<number, Lens[] | undefined>
+
 interface State {
   sequence: Step[]
+  boxes: Boxes
+  lenses: Set<string>
+}
+
+const getBoxes = (n: number): Boxes => {
+  const boxes: Boxes = {}
+
+  for (let i = 0; i < n - 1; i++) {
+    boxes[i] = []
+  }
+
+  return boxes
 }
 
 const getInfoFromStep = (text: string): StepInfo => {
@@ -55,13 +72,87 @@ const asciiHash = (text: string): number => {
   return currentValue
 }
 
+const handleEqualOperation = (state: State, step: Step) => {
+  const { label, labelHash: destinationBox, focalLength } = step
+
+  const destinationBoxContent = state.boxes[destinationBox]
+
+  if (!destinationBoxContent) {
+    return
+  }
+
+  const lensIndex = destinationBoxContent.findIndex(
+    (lens) => lens.label === step.label,
+  )
+  const lens: Lens = {
+    label,
+    focalLength,
+  }
+
+  if (lensIndex >= 0) {
+    destinationBoxContent.splice(lensIndex, 1, lens)
+  } else {
+    destinationBoxContent.push(lens)
+  }
+
+  state.boxes[destinationBox] = destinationBoxContent
+}
+
+const handleDashOperation = (state: State, step: Step) => {
+  const { labelHash: destinationBox } = step
+
+  const destinationBoxContent = state.boxes[destinationBox]
+
+  if (!destinationBoxContent) {
+    return
+  }
+
+  const lensIndex = destinationBoxContent.findIndex(
+    (lens) => lens.label === step.label,
+  )
+
+  if (lensIndex >= 0) {
+    destinationBoxContent.splice(lensIndex, 1)
+    state.boxes[destinationBox] = destinationBoxContent
+  }
+}
+
 const solution = (state: State): State => {
+  for (const step of state.sequence) {
+    const { operation } = step
+
+    if (operation === Operation["="]) {
+      handleEqualOperation(state, step)
+    } else if (operation === Operation["-"]) {
+      handleDashOperation(state, step)
+    }
+  }
+
   return state
+}
+
+const getSumOfFocusingPower = ({ boxes }: State): number => {
+  const lenses: Record<string, number> = {}
+
+  for (const [boxNumber, boxContents] of Object.entries(boxes)) {
+    if (!boxContents) {
+      continue
+    }
+
+    for (let i = 0; i < boxContents.length; i++) {
+      const { label, focalLength } = boxContents[i]
+
+      lenses[label] = (1 + parseInt(boxNumber, 10)) * (i + 1) * focalLength
+    }
+  }
+
+  return Object.values(lenses).reduce((acc, value) => acc + value, 0)
 }
 
 const parseLine = (state: State) => (line: string) => {
   state.sequence = line.split(",").map((text) => {
     const { label, operation, focalLength } = getInfoFromStep(text)
+    state.lenses.add(label)
 
     return {
       text,
@@ -80,13 +171,16 @@ const findSolution = (state: State) => () => {
     (acc, step) => acc + step.textHash,
     0,
   )
+  const sumOfFocusingPower = getSumOfFocusingPower(result)
 
-  console.log({ sumOfAllHashes })
+  console.log({ sumOfAllHashes, sumOfFocusingPower })
 }
 
 const main = async () => {
   const state: State = {
     sequence: [],
+    boxes: getBoxes(256),
+    lenses: new Set(),
   }
   const { inputFile } = await parseArgs()
 
